bea50562282dac4d6c5936ace95012cb4e4b7d99
shmalwanJHB <67931188+shmalwanJHB@users.noreply.github.com>
2020-07-10 10:14:18 +0200

    Create ft_print_alphabet.c

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_putchar(char c);
+void ft_print_alphabet (void) {
+
+    char start;
+    char end;
+
+    start = 'a';
+    end = 'z';
+    int (end = > start) {
+     ft_putchar(start);
+     start++;
+    }
+    ft_putchar('\n');
+
+}
+void ft_putchar(char c) {
+    write(1, &c, 1);
+}
+int main (void) { 
+    ft_print_alphabet();
+    return (0);
+}
