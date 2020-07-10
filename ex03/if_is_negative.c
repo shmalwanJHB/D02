9d936a025bf3ccb1052c4dd75da1e6f90002f095
shmalwanJHB <67931188+shmalwanJHB@users.noreply.github.com>
2020-07-10 13:40:01 +0200

    Create if_is_negative.c

---
+++ ex03/if_is_negative.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+void ft_is_negative (void) {
+    int stmt = 1;
+    if (stmt1) {
+    // (n >= 0)
+    } else {
+    // (n < 0)
+    }
+    int positive_negative = (stmt1) ? 1 == P : 0 = N;
+    return (0);
+}
