1b67ac41ccb05a5282e8655d2406f5bd12078727
shmalwanJHB <67931188+shmalwanJHB@users.noreply.github.com>
2020-07-15 13:04:02 +0200

    Update ft_putnbr.c
    The file is complete and has been committed and pushed.

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -1,3 +1,28 @@
-#include <unistd.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shmalwan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 12:38:27 by shmalwan          #+#    #+#             */
+/*   Updated: 2020/07/15 13:00:37 by shmalwan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include <unistd.h>
+
+void ft_putchar(char c) {
+	write(1, &c, 1);
+}
+void ft_ft(int *nbr) {
+	*nbr = 42;
+}
+int main(void) {
+	int nbr;
+	ft_ft;
+	ft_putchar((nbr / 10) + '0'); // 4
+	ft_putchar((nbr % 10) + '0'); // 2
+	ft_putchar('\n');
+	return(0);
+}
